8800814f5fdfb765a560b3af4e87267514ba1cc9
Diluvies <146093823+Diluvies@users.noreply.github.com>
2024-03-27 19:50:38 +0900

    Update getState(), move()

--- src/main/java/edu/postech/csed490h/HopingRabbitsGame.java
+++ src/main/java/edu/postech/csed490h/HopingRabbitsGame.java
@@ -102,6 +102,17 @@ public class HopingRabbitsGame {
 
         // Check if rabbit can jump (o/x and empty position in front of x/o)
         // And if rabbit can jump, jump and update game state
+        if (rabbit == Rabbit.X) {
+            movableRabbitPosition = currentGameState.indexOf("xo_");
+            if (tryUpdatingRabbitPosition(rabbit, movableRabbitPosition, movableRabbitPosition + 2)) {
+                return true;
+            }
+        } else {
+            movableRabbitPosition = currentGameState.indexOf("_xo");
+            if (tryUpdatingRabbitPosition(rabbit, movableRabbitPosition + 2, movableRabbitPosition)) {
+                return true;
+            }
+        }
 
         return false;
     }
@@ -147,6 +158,11 @@ public class HopingRabbitsGame {
      */
     String getState() {
         StringBuilder state = new StringBuilder();
+        for (Rabbit rabbit : gameState) {
+            if (rabbit == Rabbit.X) state.append("x");
+            else if (rabbit == Rabbit.O) state.append("o");
+            else state.append("_");
+        }
 
         return state.toString();
     }
